07bda071c451890bdc471bc5af01464ba7d5af73
Mike Jones <mike@netsplit.org.uk>
2022-04-03 12:37:25 +0100

    Remove extra wildcard

--- scripts/build-plugins.sh
+++ scripts/build-plugins.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob;
 
-for file in ./internal/app/plugins/**/!(*_test*).go;
+for file in ./internal/app/plugins/**/!(*_test).go;
 do
     PLUGIN=$(basename $(dirname $file));
     go build -buildmode=plugin -o ./plugins/${PLUGIN}.so $file;
